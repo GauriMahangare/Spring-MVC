bf985d95d981794cfd0aef436d147b702e936919
Gauri <gauridhumal@gmail.com>
2020-04-08 13:50:38 +0100

    Thymeleaf Integration
    Signed-off-by: Gauri <gauridhumal@gmail.com>

--- src/main/java/com/avanengineering/conference/ConferenceConfig.java
+++ src/main/java/com/avanengineering/conference/ConferenceConfig.java
@@ -1,6 +1,8 @@
 package com.avanengineering.conference;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -12,12 +14,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import java.util.Locale;
 
 @Configuration
 
 public class ConferenceConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 // Static resources like PDF or images
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -46,11 +54,37 @@ public class ConferenceConfig implements WebMvcConfigurer {
 
     @Bean
     public ViewResolver viewResolver(){
-        InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setPrefix("WEB-INF/jsp/");
-        bean.setSuffix(".jsp");
-        bean.setOrder(0);
-        return bean;
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+// Thymeleaf view resolver - 3 always below jsp resolver (Use either thymeleaf or jsp resolver and not both.
+    @Bean
+    public ViewResolver thymeleafResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(0); // Thymeleaf resolver comes first
+        return viewResolver;
+    }
+
+//Thyme Template resolver ->1
+    @Bean
+    public SpringResourceTemplateResolver  templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        return templateResolver;
+    }
+//Thyme Template engine Bean ->2
+    @Bean
+    public SpringTemplateEngine  templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
     }
 
 }
